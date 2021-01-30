f719cdc503d30203b103932163282f9cb7a33fd3
Griffin <60614863+h-griffin@users.noreply.github.com>
2021-01-29 19:41:11 -0800

    section 2

--- Section2/Composition/Composition/Program.cs
+++ Section2/Composition/Composition/Program.cs
@@ -8,7 +8,7 @@ namespace Composition
             var dbMigrator = new DbMigrator(new Logger());
 
             var logger = new Logger();
-            var installer = new Installer(logger);
+            var installer = new Installer(logger);          // <<< coposition
 
             dbMigrator.Migrate();
 
@@ -16,3 +16,25 @@ namespace Composition
         }
     }
 }
+
+// is a     inheritance
+// has a    composition
+
+// composition : one can contain another
+//      ex: car 'has an' engine
+//      installer 'has a' logger
+
+// pros of inheritance : 
+//      - code re-use
+// cons of inhereitance :
+//      - easily abused
+//      - large hierarchies
+//      - fragility
+//      - tight coupling
+
+// pros of composition :
+//      - code re-use
+//      - flexible
+//      - less coupling
+// cons of composition :
+//      - harder to understand
